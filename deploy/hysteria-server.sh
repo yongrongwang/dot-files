bd09508086bc1df4162a9b437a7d45021b76259d
yongrongwang <2278508989@qq.com>
2024-11-02 13:53:21 +0800

    add config of zsh, tmux, neovim, lazygit

---
+++ deploy/hysteria-server.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+bash -c "$(curl -fsSL https://get.hy2.sh/)"
+cd /etc/hysteria
+openssl req -x509 -nodes -newkey rsa:4096 -days 3650 -subj "/CN=amazon.com" -keyout server.key -out server.crt
+ufw allow 443/udp
+cp -f ~/.dot-files/deploy/hysteria-server.yaml config.yaml
+read "port?Port for hysteria: "
+read "pwd?Password for hysteria: "
+sed -i "s/listen:.*/listen: :$port/g" config.yaml
+sed -i "s/password:.*/password: $pwd/g" config.yaml
+chown hysteria:hysteria *
+systemctl enable --now hysteria-server.service
+systemctl status hysteria-server.service
